bfc9c02d8e771b73a7269db699d3a0c6f4ab8dba
Andrei Roi <andriyroy3@gmail.com>
2021-04-20 18:25:36 +0300

    refactor pom.xml file. log4j 2 has been added

--- src/main/java/com/controller/UrlShorterRestController.java
+++ src/main/java/com/controller/UrlShorterRestController.java
@@ -1,4 +1,5 @@
 package com.controller;
+
 import java.io.IOException;
 
 
@@ -8,6 +9,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 import com.common.ShortenUrl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,31 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UrlShorterRestController {
+    private static final Logger logger = LogManager.getLogger(UrlShorterRestController.class);
 
-    private final  Map<String, ShortenUrl> shortenUrlList = new HashMap<>();
+    private final Map<String, ShortenUrl> shortenUrlList = new HashMap<>();
 
-    @RequestMapping(value="/shortenurl", method=RequestMethod.POST)
-    public ResponseEntity<Object> getShortenUrl(@RequestBody ShortenUrl shortenUrl){
+    @RequestMapping(value = "/shortenurl", method = RequestMethod.POST)
+    public ResponseEntity<Object> getShortenUrl(@RequestBody ShortenUrl shortenUrl) {
         String randomChar = getRandomChars();
         setShortUrl(randomChar, shortenUrl);
         return new ResponseEntity<>(shortenUrl, HttpStatus.OK);
     }
 
-    @RequestMapping(value="/s/{randomstring}", method=RequestMethod.GET)
-    public void getFullUrl(HttpServletResponse response, @PathVariable("randomstring") String randomString) throws IOException {
-        response.sendRedirect(shortenUrlList.get(randomString).getFull_url());
+    @RequestMapping(value = "/s/{randomstring}", method = RequestMethod.GET)
+    public void getFullUrl(HttpServletResponse response, @PathVariable("randomstring") String randomString) {
+        try {
+            response.sendRedirect(shortenUrlList.get(randomString).getFull_url());
+
+        } catch (IOException e) {
+            logger.error("IOException in: " + e.getMessage());
+        }
     }
 
     private void setShortUrl(String randomChar, ShortenUrl shortenUrl) {
-        shortenUrl.setShort_url("http://localhost:8080/s/"+randomChar);
+        shortenUrl.setShort_url("http://localhost:8080/s/" + randomChar);
         shortenUrlList.put(randomChar, shortenUrl);
     }
 
     private String getRandomChars() {
         StringBuilder randomStr = new StringBuilder();
         String possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++) {
             randomStr.append(possibleChars.charAt((int) Math.floor(Math.random() * possibleChars.length())));
+        }
         return randomStr.toString();
     }
 
